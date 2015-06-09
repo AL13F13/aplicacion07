0e85c05aabfdb5d27fecd8f8c75d205679deb9c1
AL13F13 <dsauposafa@gmail.com>
2015-06-09 13:24:17 -0500

    doce
    se compuso el codigo

--- js/acciones.js
+++ js/acciones.js
@@ -1,36 +1,44 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	
+	$('#btnconfigurar').on('tap',function(){
+		$('#txtnombre').val($('#jugador').text());
+		});
+		
+		$('#btnguardar').on ('tap',function(){
+			var nuevonombre =$('#txtnombre').val();
+			basedatos.transaction(function(consulta){
+			consulta.executeSql("UPDATE Usuario SET NombreUsuario =? WHERE ClaveUsuario='1';",[nuevonombre]);
+			
+			});
+			cargarnombrejugador();
+			});
+	
+var basedatos=window.sqlitePlugin.openDatabase({name:"coloresBD.db",createFromLocation:1})
+	
 	cargarnombrejugador();
 	function cargarnombrejugador ()
-		{
-			basedatos.transaction(function(ejecutar){
-				var sql="SELECT NombreUsuario FROM Usuario";
+    {
+	basedatos.transaction(function(ejecutar){
+	var sql="SELECT NombreUsuario FROM usuario";
 				
 				
-				ejecutar.executesql(sql,undefined,function(ejecutar,resultado){
-					var datosjugador=resultado.rows.item(0);
-					$('#jugador').text(datosjugador.nombreusuario)
-					
-					
-					
+    ejecutar.executesql(sql, undefined, function(ejecutar, resultado){
+	
+	var datosjugador=resultado.rows.item(0);
+	$('#jugador').text(datosjugador.NombreUsuario);
 			});
 			});
-					 
-				
-				
-		}
+			}
 	
 	
 	
-	
-	
-	var basedatos=window.sqlitePlugin.openDatabase({name:"coloresBD.db",createFromLocation:1});
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('B1','audio/C.mp3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B2','audio/D.mp3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B3','audio/E.mp3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B4','audio/F.mp3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B1','audio/C.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B2','audio/D.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B3','audio/E.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B4','audio/F.pm3',function(){},function(msg){ alert ("error" + msg);});
 	
 	
 	$('#btnjugar').on('tap',function(){
@@ -64,6 +72,8 @@ document.addEventListener("deviceready",function(){
 				return q.substring (1);
 				
 			}
+			
+		
 			
 			
 			
