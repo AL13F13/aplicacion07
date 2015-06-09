3e1e255913197c59669c26da8975d7ac43e020ed
AL13F13 <dsauposafa@gmail.com>
2015-06-09 14:41:28 -0500

    quince
    prueba

--- js/acciones.js
+++ js/acciones.js
@@ -1,91 +1,73 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
-	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('B1','audio/C.pm3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B2','audio/D.pm3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B3','audio/E.pm3',function(){},function(msg){ alert ("error" + msg);});
-	audio.preloadFX('B4','audio/F.pm3',function(){},function(msg){ alert ("error" + msg);});
-	
-	var basedatos= window.sqlitePlugin.openDatabase({name: "coloresBD.db",createFromLocation:1});
+	var basedatos= window.sqlitePlugin.openDatabase({name: "coloresBD.db",createFromLocation:1})
 	cargarnombrejugador();
-	
-	
-	$('#btnconfigurar').on('tap',function(){
-		$('#txtnombre').val($('#jugador').text());
-		});
-		
-		$('#btnguardar').on ('tap',function(){
-			var nuevonombre =$('#txtnombre').val();
-			basedatos.transaction(function(consulta){
-			consulta.executeSql("UPDATE Usuario SET NombreUsuario =? WHERE ClaveUsuario='1';",[nuevonombre]);
-			
-			});
-			cargarnombrejugador();
-			});
-	
-
-	
-	
 	function cargarnombrejugador ()
-    {
-	    basedatos.transaction(function(ejecutar){
-	var sql="SELECT NombreUsuario FROM Usuario";
-				
-				
-    ejecutar.executeSql(sql, undefined, function(ejecutar,resultado){
-	
-	var datosjugador=resultado.rows.item(0);
-	$('#jugador').text(datosjugador.NombreUsuario);
+	{
+		basedatos.transaction (function(ejecutar){
+			var sql="SELECT NombreUsuario FROM Usuario";
+			ejecutar.executeSql(sql,undefined,function(ejecutar,resultado){
+				var datosjugador=resultado.rows.item(0);
+				$('#jugador').text(datosjugador.NombreUsuario)
 			});
-			});
-			}
-	
-	
-	
-	
-	
-	
+		});
+	}
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('b1','audio/C.mp3',function(){},function(msg){alert("Error "+msg);});
+	audio.preloadFX('b2','audio/D.mp3',function(){},function(msg){alert("Error "+msg);});
+	audio.preloadFX('b3','audio/E.mp3',function(){},function(msg){alert("Error "+msg);});
+	audio.preloadFX('b4','audio/F.mp3',function(){},function(msg){alert("Error "+msg);});
 	$('#btnjugar').on('tap',function(){
-		alert ("dentro");
-		var pantalla =$.mobile.getScreenHeight();
-		alert ('pantalla ' + pantalla);
-		var encabezado =$('.ui-header').outerHeight();
-		alert ('encabezado ' + encabezado);
-		var pie =$('.ui-footer').outerHeight();
-		alert ('pie' + pie);
-		var contenido =$('.ui-content').outerHeight();
-		alert ('contenido' + contenido);
-		var alto=(pantalla - encabezado - pie )/2;
-			
-		
+		var pantalla=$.mobile.getScreenHeight();
+		var encabezado=$('.ui-header').outerHeight();
+		var pie=$('.ui-footer').outerHeight();
+		var contenido=$('.ui-content').outerHeight();
+		var alto=(pantalla - encabezado - pie)/2
+		//alert('pantalla  '+ pantalla)
+		//alert('encabezado  '+ encabezado)
+		//alert('pie  '+ pie)
+		//alert('contenido  '+ contenido)
+		//alert('alto ' + alto)
 		$('.cuadro').height(alto);
 		
-		}); //cuadro
-		
-		
-		$ ('.cuadro').on('vmousedown', function (){
+	});
+	
+	//$('.cuadro').on('vmousedown',function(){
+			//$(this).addClass('pulsado');
+		//});
+		//$('.cuadro').on('vmouseup',function(){
+			//$(this).removeClass('pulsado');
+		//});
+		function quien(q)
+		{
+			audio.play(q);
+			return q.substring(1);
+		}
+		$('.cuadro').on('vmousedown',function(){
 			$('#pantalla').append(quien($(this).attr('id')));
-		$(this).addClass('pulsado');
-			}); //btnjugar
+			$(this).addClass('pulsado');
 			
-			$ ('.cuadro').on('vmouseup', function (){
-			$(this).removeClass('pulsado');
-			}); //btnjugar
-			
-			function quien (q)
-			{
-				audio.play(q);
-				return q.substring (1);
-				
-			}
-			
+		});
+	$('#btnconfigurar').on('tap',function(){
+		$('#txtnombre').val($('#jugador').text());
 		
-			
-			
-			
+	});
+	$('#btnguardar').on('tap',function(){
+	 var nuevonombre= $('#txtnombre').val();
+	 basedatos.transaction(function(consulta){
+	 consulta.executeSql("UPDATE Usuario SET NombreUsuario=? WHERE ClaveUsuario='1';",[nuevonombre]);
+	 });
+	 cargarnombrejugador()
+	});
+	function flash(boton)
+	{
+		boton.stop().animate({opacity:'0.5'},{duration:80,
+		complete:function(){
+			boton.stop().animate({opacity:'1'},200);
+		}
+		});
+	}
 }); 
 });
-
 
